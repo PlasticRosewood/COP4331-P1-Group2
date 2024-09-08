c53369ba44684c5bf04e4b511d72268e784e4a6c
SilverMight <silver@silvermight.com>
2024-09-07 21:37:50 -0400

    Add AccountController with test functionality for login

---
+++ src/controllers/AccountController.php
@@ -0,0 +1,33 @@
+<?php
+require_once(__DIR__ . '/../models/Database.php');
+require_once(__DIR__ . '/../config/db_credentials.php');
+
+class AccountController {
+    private $db;
+    
+    public function __construct() {
+        $this->db = new Database(HOST, DB, USER, PASS, CHARSET);
+    }
+
+    public function login(array $data): void {
+        if (!isset($data['username']) || !isset($data['password'])) {
+            http_response_code(400);
+            return;
+        }
+
+        $stmt = 'SELECT id FROM UserAccountInfo WHERE username = :username AND password_hash = :password';
+        $params = [
+            ':username' => $data['username'],
+            # TODO: handle hashing
+            ':password' => $data['password']
+        ];
+
+        $user = $this->db->query($stmt, $params);
+
+        if($user) {
+            # TODO: store in PHP session
+            echo $user['id'];
+        }
+    }
+}
+?>
