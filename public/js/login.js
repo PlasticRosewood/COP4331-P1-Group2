a995e31baa05240a5d65849a9c8d69fd3fbb6c2e
PlasticRosewood <jdtomjones@gmail.com>
2024-09-11 12:56:00 -0400

    pushing minor changes to run on php server on linux machine

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,45 @@
-const urlBase = "../src";
+const urlBase = '/api/auth';
 
-let username = "";
-let password = "";
+let usernameText = '';
+let passwordText = '';
 
-function doLogin() {
-    username = password = "";
+let usernameField = document.getElementById('username_field');
+let passwordField = document.getElementById('password_field');
 
-    
+// prevent page reload on form submission
+var form = document.getElementById('login_form');
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
+async function userLogin() {
+    usernameText = usernameField.value;
+    passwordText = passwordField.value;
+
+    const url = urlBase + '/login';
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: usernameText,
+                password: passwordText
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error('Response status: ' + response.status);
+        } 
+        
+        const data = await response.json();
+        console.log('Connection successful on userLogin()');
+    } catch (error) {
+        console.log('Connection failed on userLogin()');
+        console.error(error);
+    }
+
+        
 }
+
 
