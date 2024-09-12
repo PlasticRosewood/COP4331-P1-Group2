e96a19411ca371c19b942c2707782150f638e618
PlasticRosewood <jdtomjones@gmail.com>
2024-09-11 20:58:33 -0400

    temp updates to broken login page code while I merge w/ api

--- public/js/login.js
+++ public/js/login.js
@@ -20,6 +20,7 @@ async function userLogin() {
         const response = await fetch(url, {
             method: 'POST',
             headers: {
+                'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
@@ -27,13 +28,24 @@ async function userLogin() {
                 password: passwordText
             })
         });
-
+        //.then(response => console.log(response));
+        
+        
         if (!response.ok) {
             throw new Error('Response status: ' + response.status);
         } 
         
-        const data = await response.json();
-        console.log('Connection successful on userLogin()');
+        // error logging code
+        const contentType = response.headers.get('content-type');
+        if (contentType && contentType.indexOf('application/json') !== -1) {
+            const data = await response.json();
+            console.log('Connection successful on userLogin()', data);
+        } else {
+            const text = await response.text();
+            console.error('Expected JSON, got:\n', text);
+            throw new Error('Server returned non-JSON response');
+        }        
+
     } catch (error) {
         console.log('Connection failed on userLogin()');
         console.error(error);
