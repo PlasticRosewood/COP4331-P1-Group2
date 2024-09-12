a3c12d1e219c27046e464790c2806c0fbc596cbf
SilverMight <silver@silvermight.com>
2024-09-12 16:21:54 -0400

    Add token generator for auth

---
+++ src/utils/TokenGenerator.php
@@ -0,0 +1,45 @@
+<?php
+require_once(__DIR__ . '../../vendor/autoload.php');
+
+use Firebase\JWT\JWT;
+
+define(ALGORITHM, 'HS512');
+
+class TokenGenerator {
+    private string $secret;
+
+    public function __construct(string $secret) {
+        $this->secret = $secret;
+    }
+
+    public function generateToken(int $userId): string {
+        $issueTime = new DateTimeImmutable();
+        $expirationTime = $issueTime->modify('+60 minutes');
+        $serverName = "givemespace.lol";
+
+        $data = [
+            'iat' => $issueTime->getTimestamp(),
+            'iss' => $serverName,
+            'nbf' => $issueTime->getTimestamp(),
+            'exp' => $expirationTime->getTimestamp(),
+            'user_id' => $userId
+        ];
+
+        return JWT::encode($data, $this->secret, ALGORITHM);
+    }
+
+    public function getUserIdFromToken(string $token): ?int {
+        try {
+            $data = JWT::decode($token, $this->secret, algorithm);
+            return $data->user_id;
+        } catch (Exception $e) {
+            // Failed to validate token
+            return null;
+        }
+    }
+
+
+
+}
+
+?>
