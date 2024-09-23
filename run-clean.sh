e9e7b07c452a351238497250ac441ffc6d595186
Toaster <boltactiontoaster@gmail.com>
2024-09-23 17:27:46 -0400

    updated .sh to create .env if it does not exist from example

--- run-clean.sh
+++ run-clean.sh
@@ -1,2 +1,3 @@
+cp -n .env.example .env
 docker system prune -a -f
 docker compose up -d && docker-compose exec -it apache bash -c "composer install && chown -R www-data:www-data /var/www/html/vendor"
