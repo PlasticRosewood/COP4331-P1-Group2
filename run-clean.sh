8133783f5c42403d5915f7c6a644140952564106
BoltactionToaster <42529765+BoltactionToaster@users.noreply.github.com>
2024-09-23 19:37:45 -0400

    Update run-clean.sh
    Co-authored-by: Joseph Zalusky <silver@silvermight.com>

--- run-clean.sh
+++ run-clean.sh
@@ -1,3 +1,2 @@
 cp -n .env.example .env
-docker system prune -a -f
-docker compose up -d && docker-compose exec -it apache bash -c "composer install && chown -R www-data:www-data /var/www/html/vendor"
+docker compose down && docker compose up -d --build && docker-compose exec -it apache bash -c "composer install && chown -R www-data:www-data /var/www/html/vendor"
